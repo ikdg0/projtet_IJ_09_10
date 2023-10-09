63185d06c10314610d115eac95aa6e1840a6edd6
Jokast38 <115721772+Jokast38@users.noreply.github.com>
2023-10-09 17:45:47 +0200

    new
    nouveau

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 
+
+  if (!localStorage.getItem('cookiesAccepted')) {
+    if (confirm('Ce site utilise des cookies. Acceptez-vous les cookies ?')) {
+        localStorage.setItem('cookiesAccepted', true);
+    }
+}
